e5637aa8ee83dbd5c184733b74c8d70587d2a569
이정준 <junnnyyy@kakao.com>
2021-07-22 11:48:30 +0900

    spring initial setting

---
+++ cooktest/src/main/java/b206/cooktest/CooktestApplication.java
@@ -0,0 +1,13 @@
+package b206.cooktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CooktestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CooktestApplication.class, args);
+	}
+
+}
