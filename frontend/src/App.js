63c73c5b63321bd812995e260c5a979437a986c6
eypk9673 <eypk9673@naver.com>
2021-07-27 14:43:15 +0900

    S05P12B206-150 feat : FE/main 페이지 구성, appbar, sidebar, card, search

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import SearchAppBar from './components/common/SearchAppBar'
+import Card from './components/common/Card'
+import { height } from '@material-ui/system';
+import Box from '@material-ui/core/Box';
 
 function App() {
   return (
     <div className="App">
       <SearchAppBar></SearchAppBar>
+      <div style={{ height: 450 }}>Carousel</div>
+      <Box display="flex">
+
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+      </Box>
     </div>
   );
 }
