8a08ba6d78b75335f628c17a3eef97703c04785c
eypk9673 <eypk9673@naver.com>
2021-07-27 22:02:08 +0900

    S05P12B206-130 feat : recipe/FE router 설정 및 사이드바 router 삽입

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,9 @@
 import './App.css';
-import Login from './components/user/Login'
-import Main from './components/recipe/Main'
-import { Route } from 'react-router-dom';
+import Routes from './Routes'
 
 const App = () => {
   return (
-    <div className="App">
-      <Route exact path="/" component={Main}/>
-      <Route exact path="/login" component={Login}/>
-    </div>
+    <Routes/>
   );
 }
 
