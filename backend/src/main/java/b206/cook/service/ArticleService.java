6b4260e6e3e7c87ab3d567abb2e26f705a1bf837
이정준 <junnnyyy@kakao.com>
2021-07-28 16:55:13 +0900

    S05P12B206-122: article 전체 조회

--- backend/src/main/java/b206/cook/service/ArticleService.java
+++ backend/src/main/java/b206/cook/service/ArticleService.java
@@ -1,4 +1,31 @@
 package b206.cook.service;
 
+import b206.cook.domain.Article;
+import b206.cook.repository.ArticleRepository;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
 public class ArticleService {
+
+    private final ArticleRepository articleRepository;
+
+    public ArticleService(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+    // 전체 게시글 조회
+    public List<Article> findArticles() {
+        return articleRepository.findAll();
+    }
+    // 게시글 하나 조회
+    public Optional<Article> findArticle(Long articleId) {
+        return articleRepository.findById(articleId);
+    }
+    // 게시글 생성
+    // 게시글 수정
+    // 게시글 삭제
 }
