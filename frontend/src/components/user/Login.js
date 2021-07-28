f6c02636f7366a8f45506e1e6a2cd2ffd3277bfd
DongGun Ko <dongjuk157@gmail.com>
2021-07-28 11:31:07 +0900

    S05P12B206-112 feat: login ajax 구현. 테스트서버 생성후 테스트해볼것

--- frontend/src/components/user/Login.js
+++ frontend/src/components/user/Login.js
@@ -1,45 +1,77 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import { Button } from '@material-ui/core'
 import { Box } from '@material-ui/core'
-// import axios from 'axios'
+import axios from 'axios'
+
+
 
 
 const LoginForm  = styled.div`
   padding: 1rem;
   border: solid;
-  border-radius: 0.5rem;
-  
+  border-radius: 0.5rem; 
 `
 
-const onLogin = () => {
-  alert("login! 아직 통신 구현 안했다~~~~")
-  //ajax 통신 구현해야함
-  // axios.post()
-}
+const Login = ({history}) => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  
+  const onLogin = () => {
+    console.error("BASE_URL 설정하지 않음")
+    // Ajax 통신 post 
+    const BASE_URL = process.env.REACT_APP_API_URL
+    const PORT = process.env.REACT_APP_API_PORT
+    const config = {
+      method: 'post',
+      url: `${BASE_URL}:${PORT}/login`,
+      data: {
+        email,
+        password,
+      }
+    }
+    axios(config)
+      .then( res => {
+      alert(`${res}/ login!`)
+      //modal로 구현할경우 아래를 제거하고 로그인 상태를 넘겨주면됨
+      history.push('/')
+    }).catch( e => {
+      console.log(e)
+    })
+  }
 
-const Login = () => {
   return (
     <LoginForm>
       <h1>LOGIN</h1>
-      <form>
-        <div>
-          <p>Email</p>
-          <input type="email" placeholder="이메일 주소를 입력하세요"></input>
-        </div>
-        <div>
-          <p>Password</p>
-          <input type="password" placeholder="비밀번호를 입력하세요"></input>
-        </div>
-          <Box mt={2}>
-            <Button 
-              onClick={onLogin}
-              variant="contained"
-              color="primary"
-            >
-              Submit
-            </Button>
-          </Box>
+      <form onSubmit={onLogin}>
+        <label htmlFor="email">
+          Email:
+          <input 
+            type="email" placeholder="이메일 주소를 입력하세요"
+            id="email"
+            value={email}
+            onChange={ e => setEmail(e.target.value)}
+          ></input>
+        </label>
+        <br />
+        <label htmlFor="password">
+          Password: 
+          <input 
+            type="password" placeholder="비밀번호를 입력하세요"
+            id="password"
+            value={password}
+            onChange={ e => setPassword(e.target.value)}
+          ></input>
+        </label>
+        <Box mt={2}>
+          <Button 
+            onClick={onLogin}
+            variant="contained"
+            color="primary"
+          >
+            Submit
+          </Button>
+        </Box>
       </form>
     </LoginForm>
   )
