3bb139e1ac64601ed4e494deaf04086c6bfffae8
Donggun Ko <dongjuk157@gmail.com>
2021-08-03 17:17:43 +0900

    feat: kakaoLogin ajax

--- frontend/src/components/user/KakaoOAuthHandler.js
+++ frontend/src/components/user/KakaoOAuthHandler.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import axios from "axios"
 import { CircularProgress } from "@material-ui/core"
 
@@ -6,22 +6,30 @@ const KakaoOAuthHandler = ({history}) => {
   const code = new URL(window.location.href).searchParams.get("code")
   const BASE_URL = process.env.REACT_APP_API_URL
   const PORT = process.env.REACT_APP_API_PORT
-  const config = {
-    method: 'get',
-    url: `${BASE_URL}:${PORT}/oauth/callback/kakao?code=${code}`,
-  }
+  
   // console.log(code)
-  axios(config)
-    .then(res => {
-      // console.log(res)
-      const ACCESS_TOKEN = res.data.ACCESS_TOKEN
-      localStorage.setItem("token", ACCESS_TOKEN)
-      history.push('/')
-  }).catch(e =>{
-    // console.log(e)
-    history.push("/login")
-  })
-
+  useEffect( () => {
+    async function SubmitServer() {
+      const config = {
+        method: 'get',
+        url: `${BASE_URL}:${PORT}/oauth/callback/kakao?code=${code}`,
+      }
+      // console.log(config)
+      await axios(config)
+      .then(res => {
+        console.log(res)
+        const ACCESS_TOKEN = res.data.accessToken
+        localStorage.setItem("token", ACCESS_TOKEN)
+        history.push('/')
+      }).catch(e =>{
+        // console.log(e)
+        history.push("/login")
+      })
+    }
+    SubmitServer()
+    console.log("end")
+  }, [])
+    
   return (
     <CircularProgress />
   )
