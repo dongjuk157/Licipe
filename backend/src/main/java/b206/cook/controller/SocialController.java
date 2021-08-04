ffa41507f644af2052b6fefdf301e4307a4ac47e
안창호 <aysul0@naver.com>
2021-08-04 17:29:07 +0900

    S05P13B206-79 feat : 토큰 받기

--- backend/src/main/java/b206/cook/controller/SocialController.java
+++ backend/src/main/java/b206/cook/controller/SocialController.java
@@ -1,18 +1,33 @@
 package b206.cook.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import b206.cook.service.KakaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URISyntaxException;
 
 @CrossOrigin(origins = { "*" }, maxAge = 6000)
-@Controller
+@RestController
 public class SocialController {
 
+    private final KakaoService kakaoService;
+
+    @Autowired
+    public SocialController(KakaoService kakaoService) {
+        this.kakaoService = kakaoService;
+    }
+
     @GetMapping("/oauth/callback/kakao")
-    public @ResponseBody String kakaoCallback(@RequestParam String code) { // Data를 리턴해주는 컨트롤러 함수
-        System.out.println("인증 완료됨. 코드값:" + code);
+    public String kakaoCallback(@RequestParam String code) throws URISyntaxException { // Data를 리턴해주는 컨트롤러 함수
+
+        String access_Token = "";
+        try{
+//            System.out.println("code = " + code);
+            access_Token = kakaoService.getAccessToken(code);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return "인증 완료";
     }
 }
