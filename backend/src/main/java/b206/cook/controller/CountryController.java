418049f4a2bd052084b6881cf95faf470fb097f1
이정준 <junnnyyy@kakao.com>
2021-08-04 11:28:14 +0900

    S05P13B206-87 feat: countries

--- backend/src/main/java/b206/cook/controller/CountryController.java
+++ backend/src/main/java/b206/cook/controller/CountryController.java
@@ -1,4 +1,26 @@
 package b206.cook.controller;
 
+import b206.cook.domain.Country;
+import b206.cook.service.CountryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class CountryController {
+
+    private final CountryService countryService;
+
+    @Autowired
+    public CountryController(CountryService countryService) {
+        this.countryService = countryService;
+    }
+
+    @GetMapping("/foods/countries")
+    public List<Country> list() {
+        return countryService.findCountries();
+    }
+
 }
