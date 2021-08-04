5c2b30cde7366e30b6f687b28af2cc436e938b66
이정준 <junnnyyy@kakao.com>
2021-08-04 11:44:01 +0900

    S05P13B206-59 feat: situaiton

--- backend/src/main/java/b206/cook/service/SituationService.java
+++ backend/src/main/java/b206/cook/service/SituationService.java
@@ -1,4 +1,20 @@
 package b206.cook.service;
 
+import b206.cook.domain.Situation;
+import b206.cook.repository.SituationRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class SituationService {
+    private final SituationRepository situationRepository;
+
+    public SituationService(SituationRepository situationRepository) {
+        this.situationRepository = situationRepository;
+    }
+
+    public List<Situation> findSituations() {
+        return situationRepository.findAll();
+    }
 }
