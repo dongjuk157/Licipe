bd2d16c60e15da43fb9a1c91aad805c3ace76795
이정준 <junnnyyy@kakao.com>
2021-08-05 11:21:22 +0900

    server address

--- backend/src/main/java/b206/cook/config/SecurityConfig.java
+++ backend/src/main/java/b206/cook/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         final CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.addAllowedOrigin("*");
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
         configuration.setAllowCredentials(true);
