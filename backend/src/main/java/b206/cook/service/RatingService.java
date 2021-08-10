0bd8ad21fd096b76ea93112a8736425776ac6b6d
이정준 <junnnyyy@kakao.com>
2021-08-10 11:23:40 +0900

    S05P13B206-185 feat: Rating-service/be

--- backend/src/main/java/b206/cook/service/RatingService.java
+++ backend/src/main/java/b206/cook/service/RatingService.java
@@ -1,8 +1,14 @@
 package b206.cook.service;
 
+import b206.cook.domain.entity.Rating;
 import b206.cook.domain.repository.RatingRepository;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Optional;
+
+@Service
 @Transactional
 public class RatingService {
 
@@ -12,6 +18,15 @@ public class RatingService {
         this.ratingRepository = ratingRepository;
     }
 
+    public Rating rate(Rating rating) {
+        return ratingRepository.save(rating);
+    }
 
+    public Optional<Rating> findRatingById(Long ratingId) {
+        return ratingRepository.findById(ratingId);
+    }
 
+    public List<Rating> ratings(String snsId) {
+        return ratingRepository.findByMember(snsId);
+    }
 }
