d79649cf8bce4870b83af45f40965f6b56f7e392
이정준 <junnnyyy@kakao.com>
2021-08-17 16:00:44 +0900

    S05P13B206-253 feat: RecipeSubCategory 스타일링-디자인

--- backend/src/main/java/b206/cook/domain/repository/JpaFoodMemberRepository.java
+++ backend/src/main/java/b206/cook/domain/repository/JpaFoodMemberRepository.java
@@ -18,4 +18,12 @@ public class JpaFoodMemberRepository implements FoodMemberRepository{
                 .setParameter("snsId", snsId)
                 .getResultList();
     }
+
+    @Override
+    public List<Food_Member> findRecentClips(String snsId) {
+        return em.createQuery("select fm from Food_Member fm where fm.member.snsId = :snsId and fm.isClip=true order by fm.id desc", Food_Member.class)
+                .setParameter("snsId", snsId)
+                .setMaxResults(5)
+                .getResultList();
+    }
 }
