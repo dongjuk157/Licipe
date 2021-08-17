ce2f2cd60f05dfe3e1202181734407454d626aaf
DongGun Ko <dongju0628@naver.com>
2021-08-17 16:18:18 +0900

    S05P13B206-260 S05P13B206-261 S05P13B206-262 feat: 마이 페이지 레이아웃 변경

--- frontend/src/components/user/MyRatings.js
+++ frontend/src/components/user/MyRatings.js
@@ -1,12 +1,9 @@
 import React, { useEffect } from 'react'
 import SearchAppBar from '../common/SearchAppBar'
-import styled from 'styled-components'
 import { useDispatch, useSelector } from 'react-redux'
 import * as userActions from "../../redux/modules/user";
+import Card2 from '../common/Card2';
 
-const MyPageContainer = styled.div`
-  padding: 1rem;
-`
 
 const MyRatings = () => {
   const dispatch = useDispatch()
@@ -23,21 +20,22 @@ const MyRatings = () => {
   return (
     <>
       <SearchAppBar />
-      <MyPageContainer>
-        <h1>MyRatings</h1>
-        <ul>
+      <div className="p-3">
+        <h1>제 점수는요?</h1>
+        <div className="row">
         { ratingList.length !== 0 ? (
-            ratingList.map((element) => {
-              // console.log(element)
-              return (<li key={element.id}>
-                음식: {element.food.name} | 점수: {element.score} | 
-              </li>)
+            ratingList.map((item, index) => {
+              return (
+                <div className="col-12 col-sm-6 col-md-4 col-lg-3">
+                  <Card2 item={item} key={index}></Card2>
+                </div>
+              )
             })
         ) : (
           <li>평가한 레시피가 없어요...</li>)
         }
-        </ul>
-      </MyPageContainer>
+        </div>
+      </div>
     </>
   )
 }
