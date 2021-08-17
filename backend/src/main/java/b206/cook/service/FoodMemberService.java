df84fd113eaee0a3d0c5868fed131d17ded9b2b8
이정준 <junnnyyy@kakao.com>
2021-08-17 20:51:46 +0900

    S05P13B206-270 feat: RecipeInfo 스타일링-레이아웃

--- backend/src/main/java/b206/cook/service/FoodMemberService.java
+++ backend/src/main/java/b206/cook/service/FoodMemberService.java
@@ -1,13 +1,16 @@
 package b206.cook.service;
 
+import b206.cook.domain.dto.FoodMemberSaveRequestDto;
 import b206.cook.domain.entity.Food_Member;
 import b206.cook.domain.entity.Member;
 import b206.cook.domain.repository.FoodMemberRepository;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
+@Transactional
 public class FoodMemberService {
 
     private final FoodMemberRepository foodMemberRepository;
@@ -21,5 +24,17 @@ public class FoodMemberService {
     }
     public List<Food_Member> findRecentClippedFoods(String snsId) {
         return foodMemberRepository.findRecentClips(snsId);
+    }
+    public Food_Member saveClip(FoodMemberSaveRequestDto foodMemberSaveRequestDto) {
+        validateDuplicateClip(foodMemberSaveRequestDto.getFood().getId(), foodMemberSaveRequestDto.getMember().getId());
+        return foodMemberRepository.save(foodMemberSaveRequestDto.toEntity());
+    }
+
+    // 중복
+    private void validateDuplicateClip(Long foodId, Long memberId) {
+        foodMemberRepository.findFoodMember(foodId, memberId)
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 클립한 레시피 입니다.");
+                });
     }
 }
