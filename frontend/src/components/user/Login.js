903ce16485c36419d01e6306639b21fd6d02e793
DongGun Ko <dongju0628@naver.com>
2021-08-17 23:10:02 +0900

    feat: add app bar on login page

--- frontend/src/components/user/Login.js
+++ frontend/src/components/user/Login.js
@@ -1,37 +1,40 @@
 import React from 'react'
 import { Button, Grid } from '@material-ui/core'
 import KakaoLogin from './KakaoLogin'
-
+import SearchAppBar from '../common/SearchAppBar'
 
 
 const Login = ({history}) => {
   return (
-    <Grid
-      container
-      spacing={0}
-      direction="column"
-      alignItems="center"
-      justifyContent="center"
-    >
-      <h1>LOGIN</h1>
+    <>
+      <SearchAppBar></SearchAppBar>
       <Grid
         container
+        spacing={0}
         direction="column"
         alignItems="center"
         justifyContent="center"
       >
-        <KakaoLogin />
-        <Button disabled>
-          Google
-        </Button>
-        <Button  disabled>
-          Naver
-        </Button>
-        <Button disabled onClick={()=>{history.push('/login/email')}}>
-          Email로 로그인
-        </Button>
+        {/* <h1>LOGIN</h1> */}
+        <Grid
+          container
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <KakaoLogin />
+          <Button disabled>
+            Google
+          </Button>
+          <Button  disabled>
+            Naver
+          </Button>
+          <Button disabled onClick={()=>{history.push('/login/email')}}>
+            Email로 로그인
+          </Button>
+        </Grid>
       </Grid>
-    </Grid>
+    </>
   )
 }
 
