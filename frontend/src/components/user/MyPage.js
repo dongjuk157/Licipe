abbcc59e3ccfcb8d51a4a70e30a44bd18f59cbd3
DongGun Ko <dongju0628@naver.com>
2021-08-18 10:43:09 +0900

    feat: mypage layout 변경

--- frontend/src/components/user/MyPage.js
+++ frontend/src/components/user/MyPage.js
@@ -11,7 +11,11 @@ const MyPage = ({match}) => {
   const history = useHistory()
   const dispatch = useDispatch()
   const loggedInfo = storage.get('loggedInfo'); // 로그인 정보
-  const result = useSelector((state) => state.user.get('result')).toJS()
+  // const result = useSelector((state) => state.user.get('result')).toJS()
+  const myClips = useSelector((state) => state.user.getIn(['articles', 'clips'])).toJS()
+  const myArticles = useSelector((state) => state.user.getIn(['articles', 'articles'])).toJS()
+  const myRatings = useSelector((state) => state.user.getIn(['articles', 'ratings'])).toJS()
+  
   
   useEffect(()=>{
     if(!loggedInfo) {
@@ -20,8 +24,16 @@ const MyPage = ({match}) => {
     async function getClips () {
       await dispatch(userActions.getUserClipsRecent())
     }
+    async function getRatings () {
+      await dispatch(userActions.getUserRatingsRecent())
+    }
+    async function getArticles () {
+      await dispatch(userActions.getUserArticlesRecent())
+    }
     getClips()
-    return dispatch(userActions.initializeForm('result'))
+    getRatings()
+    getArticles()
+    return dispatch(userActions.initializeForm('articles'))
   }, [match.params.url])
 
   if(!loggedInfo) { // 로그인 정보가 없으면 로그인페이지 전환
@@ -29,7 +41,9 @@ const MyPage = ({match}) => {
     history.push('/login')
     return
   } 
-  const clipedList = Object.assign(result)
+  const clipedList = Object.assign(myClips)
+  const articleList = Object.assign(myArticles)
+  const ratingList = Object.assign(myRatings)
 
 
   const editUserProfile = () => {
@@ -51,8 +65,9 @@ const MyPage = ({match}) => {
   }
   const scrapedRecipe = () => {
     history.push('/myclips')
-
   }
+
+
   return (
     <>
       <SearchAppBar />
@@ -62,14 +77,14 @@ const MyPage = ({match}) => {
             onClick={editUserProfile} disabled
             className="m-1"
           >유저 정보 관리</Button>
-          <Button variant="dark" 
+          {/* <Button variant="dark" 
             onClick={postedArticle}
             className="m-1"
-          >인증한 글</Button>
-          <Button variant="dark"
+          >인증한 글</Button> */}
+          {/* <Button variant="dark"
             onClick={evaluatedRecipe}
             className="m-1"
-          >평가한 요리</Button>
+          >평가한 요리</Button> */}
         
         </div>
         <div className="d-flex flex-column p-2">
@@ -89,6 +104,46 @@ const MyPage = ({match}) => {
               })
             ) : (
               <p>스크랩한 레시피가 없어요...</p>
+            )}
+          </div>
+        </div> 
+        <div className="d-flex flex-column p-2">
+          <div className="d-flex justify-content-between align-items-center">
+            <h3 className="d-inline"> 인증한 글</h3>
+            <span onClick={postedArticle} style={{cursor:"pointer"}} className="mb-0">전체보기</span>
+          </div>
+          <div className="row">
+            {  articleList.length !== 0 ? (
+              articleList.map((item, index)=>{
+                return (
+                  // 슬라이더 있으면 좋을듯
+                  <div key={index} className="col-12 col-sm-6 col-md-4 col-lg-3" >
+                    <Card2 item={item}></Card2>
+                  </div>
+                )
+              })
+            ) : (
+              <p>자랑한 글이 없어요...</p>
+            )}
+          </div>
+        </div> 
+        <div className="d-flex flex-column p-2">
+          <div className="d-flex justify-content-between align-items-center">
+            <h3 className="d-inline">평가한 레시피</h3>
+            <span onClick={evaluatedRecipe} style={{cursor:"pointer"}} className="mb-0">전체보기</span>
+          </div>
+          <div className="row">
+            {  ratingList.length !== 0 ? (
+              ratingList.map((item, index)=>{
+                return (
+                  // 슬라이더 있으면 좋을듯
+                  <div key={index} className="col-12 col-sm-6 col-md-4 col-lg-3" >
+                    <Card2 item={item}></Card2>
+                  </div>
+                )
+              })
+            ) : (
+              <p>평가한 레시피가 없어요...</p>
             )}
           </div>
         </div> 
