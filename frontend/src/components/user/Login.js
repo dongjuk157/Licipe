1217090c00ee4a4d34ae03141783bc933d9d3fe2
eypk9673 <eypk9673@naver.com>
2021-08-19 20:42:40 +0900

    design

--- frontend/src/components/user/Login.js
+++ frontend/src/components/user/Login.js
@@ -2,7 +2,9 @@ import React from 'react'
 import { Button, Grid } from '@material-ui/core'
 import KakaoLogin from './KakaoLogin'
 import SearchAppBar from '../common/SearchAppBar'
-
+import kakao_button from '../../style/icon/kakao_login_large_wide.png';
+import naver_button from '../../style/icon/btnW_완성형.png';
+import google_button from '../../style/icon/btn_google_signin_light_normal_web@2x.png';
 
 const Login = ({history}) => {
   return (
@@ -23,11 +25,12 @@ const Login = ({history}) => {
           justifyContent="center"
         >
           <KakaoLogin />
-          <Button disabled>
-            Google
-          </Button>
+          
           <Button  disabled>
-            Naver
+            <img src={naver_button} className='social'></img>
+          </Button>
+          <Button disabled>
+            <img src={google_button} className='social'></img>
           </Button>
           <Button disabled onClick={()=>{history.push('/login/email')}}>
             Email로 로그인
